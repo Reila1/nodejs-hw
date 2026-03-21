b0a4a7a41aff2688b5b3bd26140bb67669159e31
Sofiia Moiseienko <sofiamouseenko@gmail.com>
2026-03-21 17:06:33 +0200

    serd

---
+++ src/server.js
@@ -0,0 +1,29 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import notesRoutes from './routes/notesRoutes.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(logger);
+app.use(cors());
+app.use(express.json());
+
+app.use(notesRoutes);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+const startServer = async () => {
+  await connectMongoDB();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
+
+startServer();
