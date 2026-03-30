8e7cea97b45ea54bc9438d36710ab5770a40a36d
Sofiia Moiseienko <sofiamouseenko@gmail.com>
2026-03-30 12:22:36 +0300

    remove node_modules and .env from git

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
@@ -16,6 +17,7 @@ app.use(express.json());
 
 app.use(notesRoutes);
 
+app.use(errors());
 app.use(notFoundHandler);
 app.use(errorHandler);
 
